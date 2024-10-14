b85ec2437f9501553d6be0089d4437dc80de4182
Samy Akli <samyakli685@gmail.com>
2024-10-14 12:33:38 +0100

    HTML to Blade with CSS Assets

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-
-Route::get('/second', function() {
-    return view('second');
-});
-# ---------OR--------- #
-# Route::view('/second', 'second');
