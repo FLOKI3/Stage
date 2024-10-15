43c0824be17b6081c58d23e5b31e23a9660972e6
Samy Akli <samyakli685@gmail.com>
2024-10-15 17:58:53 +0100

    Route Parameters and Route Model Binding

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::view('/', 'home')->name('home');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/about', 'about')->name('about');
+
+Route::get('posts/{post}', [PostController::class, 'show'])->name('post.show');
